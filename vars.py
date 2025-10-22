b6018609c238a9aab6b057e2831c9ba7d53b8c96
prubac <prubac@sgh.waw.pl>
2025-10-22 16:32:46 +0200

    int and float vars

---
+++ vars.py
@@ -0,0 +1,28 @@
+a = 673519852782195721975982525235243623623621621621628562673491665215215215215215
+print(f'a={a}')
+print(type(a))
+f = 25253451256216362362362.362367665
+#print(f'f={f}')
+print('f={}'.format(f))
+print('f=' + str(f))
+print('f=',f)
+print(type(f))
+
+g = 0.000000000000000000000000000000002552525216216236263623
+print('g={}'.format(g))
+
+b = 1.79e308
+print('b={}'.format(b))
+d = 10
+bd = b * d
+print('bd={}'.format(bd))
+
+s = '252525.356547'
+print('s={}'.format(s))
+print(type(s))
+sf = float(s)
+print('sf={}'.format(sf))
+print(type(sf))
+
+si = int(s)
+print('si={}'.format(si))
