ed6c8ed47a3af4efafe618846f0e276c6af4836c
prubac <prubac@sgh.waw.pl>
2025-10-29 14:25:48 +0100

    conditions - truth table

---
+++ conditions.py
@@ -0,0 +1,21 @@
+p = True
+q = True
+print(f'p={p} q={q}')
+
+if p != True:
+    print('p!=True')
+
+if p and q:
+    print('p and q')
+else:
+    print('not p or not q')
+
+if p or q:
+    print('p or q')
+else:
+    print('not p and not q')
+
+if p ^ q:
+    print('p XOR q')
+else:
+    print('not p XOR q')
