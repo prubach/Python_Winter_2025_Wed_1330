48906a113c687ca59dea98f458aa709640e7f23e
prubac <prubac@sgh.waw.pl>
2025-10-29 16:06:19 +0100

    Sum list 2D

---
+++ lists_2D.py
@@ -0,0 +1,20 @@
+num_list = [[2, 4],
+            [6, 7],
+            [8, 9]]
+print(num_list)
+print(num_list[1])
+print(num_list[1][0])
+num_list[1][0] = 90
+print(num_list)
+
+def sum_rows(l1):
+    #
+    return ...
+
+def sum_cols(l1):
+    #
+    return ...
+
+def sum_all(l1):
+    #
+    return ...
