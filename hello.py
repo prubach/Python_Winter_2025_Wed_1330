e42221d887e741a69fd9ac351001dfd18701fa93
prubac <prubac@sgh.waw.pl>
2025-11-05 15:00:50 +0100

    Test performance of numpy vs python for summing rows/cols

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
-a = 100
-print(f'a={a}')
-print('Hello')
-b = 50
-print(f'b={b}')
-c = 24542
-print(c)
-d = 141
+if __name__=='__main__':
+    a = 100
+    print(f'a={a}')
+    print('Hello')
+    b = 50
+    print(f'b={b}')
+    c = 24542
+    print(c)
+    d = 141
--- lists_2D.py
+++ lists_2D.py
@@ -1,20 +1,53 @@
-num_list = [[2, 4],
-            [6, 7],
-            [8, 9]]
-print(num_list)
-print(num_list[1])
-print(num_list[1][0])
-num_list[1][0] = 90
-print(num_list)
+import numpy as np
 
 def sum_rows(l1):
-    #
-    return ...
+    return [ sum(row) for row in l1 ]
 
 def sum_cols(l1):
-    #
-    return ...
+    res_sum = []
+    for i in range(len(l1[0])):
+        col_sum = 0
+        for j in range(len(l1)):
+            col_sum += l1[j][i]
+        res_sum.append(col_sum)
+    return res_sum
+
+def sum_cols_zip(l1):
+    print(list(zip(*l1)))
+    return sum_rows(list(zip(*l1)))
+
 
 def sum_all(l1):
-    #
-    return ...
+    return sum(sum_rows(l1))
+
+def sum_cols_rows(l1):
+    return sum_rows(l1), sum_cols(l1), sum(sum_rows(l1))
+
+def sum_np_cols_rows(l1):
+    row_sum = np.sum(l1, axis=1)
+    col_sum = np.sum(l1, axis=0)
+    return row_sum.tolist(), col_sum.tolist(), int(np.sum(l1))
+
+
+if __name__=='__main__':
+    num_list = [[2, 4],
+                [6, 7],
+                [8, 9]]
+    print(num_list)
+    print(num_list[1])
+    print(num_list[1][0])
+    num_list[1][0] = 90
+    print(num_list)
+    print('sum rows:')
+    print(sum_rows(num_list))
+    print('sum cols:')
+    print(sum_cols(num_list))
+    print('sum all:')
+    print(sum_all(num_list))
+
+    print('sum cols zip:')
+    print(sum_cols_zip(num_list))
+    print('sum using numpy:')
+    print(sum_cols_rows(num_list))
+    print(sum_np_cols_rows(num_list))
+
