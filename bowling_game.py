538323d92e83f7fedc7131031d18749167108f1b
prubac <prubac@sgh.waw.pl>
2025-11-12 15:55:00 +0100

    Bowling game - solutions

--- bowling_game.py
+++ bowling_game.py
@@ -10,7 +10,18 @@ n - number of rows
 How many bowls are necessary given the "n"
 """
 
-def count_bowls(n):
+def count_bowls_loop(n):
     bowls_count = 0
-    #TODO ...
+    for bowl in range(1, n + 1):
+        bowls_count += bowl
     return bowls_count
+
+def count_bowls_range(n):
+    return sum(range(1, n+1))
+
+def count_bowls_sequence(n):
+    return round((n * (n + 1))/2)
+
+print(f'Method: count_bowls_loop: {count_bowls_loop(6)}')
+print(f'Method: count_bowls_range: {count_bowls_range(6)}')
+print(f'Method: count_bowls_sequence: {count_bowls_sequence(6)}')
